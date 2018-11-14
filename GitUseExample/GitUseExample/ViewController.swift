d04a63d18b5407224269646b733cbf766f3329f5
Brooks, Hunter T <129878@s020rmc148-21.local>
2018-11-14 11:29:59 -0600

    log message added button with its method

--- GitUseExample/GitUseExample/ViewController.swift
+++ GitUseExample/GitUseExample/ViewController.swift
@@ -19,7 +19,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a git tutorial" )
+    }
+    
 }
 
