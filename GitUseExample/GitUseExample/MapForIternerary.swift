99446cd1fe079d1c60db5649a56f29bd8d1f4357
Brooks, Hunter T <129878@s020rmc148-21.local>
2018-11-14 12:01:02 -0600

    la la la la la

---
+++ GitUseExample/GitUseExample/MapForIternerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForIternerary.swift
+//  GitUseExample
+//
+//  Created by Brooks, Hunter T on 11/14/18.
+//  Copyright © 2018 Brooks, Hunter T. All rights reserved.
+//
+
+import Foundation
